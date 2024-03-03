327f1f325fd3e05f784915d3579b5678e3ec6aaa
navidalvand <navidalvand@gmail.com>
2024-03-03 23:45:16 +0330

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("hello world\n");
+    printf("---------------------------------------------------------------------\n");
+    printf("yeahhhh bitch\n");
+    return 0;
+}
