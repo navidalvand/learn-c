1e27ae6abc1f00323d68c1a7ca2554b2707f3f2b
navidalvand <navidalvand@gmail.com>
2024-03-07 23:45:32 +0330

    int char char[] float and more...

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
 int main () {
-    printf("hello world\n");
-    printf("---------------------------------------------------------------------\n");
-    printf("yeahhhh bitch\n");
+
+    int x = 11;
+    int y;
+    y = 24;
+
+    int age = 22;
+
+    float sallary = 1500.00;
+
+    char one = 'C';
+
+    char firstName[] = "navid";
+    char lastName[] = "alvand";
+
+    printf("my first name is %s\n", firstName);
+    printf("my last name is %s\n", lastName);
+    printf("one is %c\n", one);
+    printf("my sallary is %f\n", sallary);
+    printf("my age is %d\n", age);
+
+    
+
     return 0;
 }
